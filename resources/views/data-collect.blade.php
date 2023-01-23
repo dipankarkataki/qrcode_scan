a30245958b0b5c8387e6d8e852c717c731c40b74
dipankar kataki <dipankar.kataki@ekodus.com>
2023-01-23 17:06:45 +0530

    stayfit new update

--- resources/views/data-collect.blade.php
+++ resources/views/data-collect.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,41 +13,50 @@
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"
+        integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+
     <style>
-       body{
+        /* body{
             background-image: url('back2.jpg');
             background-size: cover;
             background-repeat: no-repeat;
-            height:100vh;
-        }
-        .container{
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            width:50%;
-        }
-        .user-data-form{
+            height:100%;
+        } */
+        .user-data-form {
             border: 1px solid #efecec;
             box-shadow: 0px 4px 10px #e8e4e4;
-            background-color: white;
+            background-color: #ffffff;
             padding: 20px;
             border-radius: 5px;
         }
-        .logo{
+
+        .logo {
             display: flex;
             flex-direction: row;
             justify-content: center;
             align-items: center;
         }
-        .logo img{
+
+        .logo img {
             height: 100px;
         }
 
-        button{
+        .banner img {
+            width: 100%;
+            height: auto;
+        }
+
+        button {
             border-radius: 25px;
             padding-left: 30px;
             padding-right: 30px;
@@ -60,117 +70,177 @@
             font-size: 12px;
         }
 
-        label{
-            font-size:14px;
-            font-weight:500;
+        label {
+            font-size: 14px;
+            font-weight: 500;
         }
 
-        ::placeholder{
-            font-size:12px;
+        ::placeholder {
+            font-size: 12px;
         }
-
-        @media only screen and (max-width: 600px) {
-            .container{
-                width:90%;
+        .small-screen{
+            transition: 0.6s;
+            display:none;
+        }
+        @media only screen and (max-width:767px){
+            .big-screen{
+                transition: 0.6s;
+                display:none;
             }
 
-            label{
-                font-size:12px;
-                font-weight:500;
+            .small-screen{
+                transition: 0.6s;
+                display:block;
+            }
+            .wrap .img{
+                background-position: bottom;
             }
         }
-        
+
     </style>
 </head>
+
 <body>
-    <div class="container">
-        <div class="user-data-form">
-            <div class="logo">
-                <img src="/logo/stayfit-logo.png" alt="logo">
-                {{-- <h3>Brand Logo</h3> --}}
+
+    <section class="ftco-section">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 col-lg-10">
+                    <div class="wrap d-md-flex">
+                        <div class="img small-screen" style="background-image: url(bg5.jpg);"></div>
+                        <div class="img big-screen" style="background-image: url(bg3.jpg);"></div>
+                        <div class="login-wrap p-4 p-md-5">
+                            <div class="d-flex">
+                                <div class="w-100 d-flex justify-content-center align-items-center ">
+                                    <h5 class="mb-4 small-screen" style="font-family: 'Secular One', sans-serif;">THE STAYFIT KITCHEN</h5>
+                                   <img src="/logo/stayfit-logo.png" alt="" height="100" class="big-screen">
+                                </div>
+                            </div>
+                            <form id="dataCollectForm" class="form-horizontal">
+                                @csrf
+                                <div class="mb-3 mt-3">
+                                    <label for="name" class="form-label">Enter Full Name:</label>
+                                    <input type="text" class="form-control" id="name" placeholder="e.g Jhon Doe" name="name" required>
+                                </div>
+                                <div class="mb-3 mt-3">
+                                    <label for="phone" class="form-label">Enter Mobile Number ( Please Provide WhatsApp Only ):</label>
+                                    <input type="text" class="form-control" id="phone" placeholder="e.g 7896541230" name="phone" pattern="^[6789]\d{9}$" title="Phone Number Must Start With The Digit 6,7,8,9" required>
+                                </div>
+                                <div class="mb-3 mt-3">
+                                    <label for="email" class="form-label">Enter Email:</label>
+                                    <input type="email" class="form-control" id="email" placeholder="e.g jhondoe@xyz.com" name="email" required>
+                                </div>
+                                <div class="mb-3 mt-3">
+                                    <label for="city" class="form-label">Enter City:</label>
+                                    <input type="text" class="form-control" id="city" placeholder="e.g Guwahati" name="city" required>
+                                </div>
+                                <div class="mb-3 mt-4 text-center">
+                                    <button id="continueBtn" type="submit">CONTINUE</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <form id="dataCollectForm" class="form-horizontal">
-                @csrf
-                <div class="mb-3 mt-3">
-                    <label for="name" class="form-label">Enter Full Name:</label>
-                    <input type="text" class="form-control" id="name" placeholder="e.g Jhon Doe" name="name" required>
-                </div>
-                <div class="mb-3 mt-3">
-                    <label for="phone" class="form-label">Enter Mobile Number ( Please Provide WhatsApp Only ):</label>
-                    <input type="text" class="form-control" id="phone" placeholder="e.g 7896541230" name="phone" pattern="^[6789]\d{9}$" title="Phone Number Must Start With The Digit 6,7,8,9" required>
-                </div>
-                <div class="mb-3 mt-3">
-                    <label for="email" class="form-label">Enter Email:</label>
-                    <input type="email" class="form-control" id="email" placeholder="e.g jhondoe@xyz.com" name="email" required>
-                </div>
-                <div class="mb-3 mt-3">
-                    <label for="city" class="form-label">Enter City:</label>
-                    <input type="text" class="form-control" id="city" placeholder="e.g Guwahati" name="city" required>
-                </div>
-                <div class="mb-3 mt-4 text-center">
-                    <button id="continueBtn" type="submit">CONTINUE</button>
-                </div>
-            </form>
         </div>
-    </div>
+    </section>
+    {{-- <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12">
+                <div class="banner">
+                    <img src="/yoga.jpg" alt="logo">
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12">
+                <div class="user-data-form">
+                    <div class="logo">
+                        <img src="/logo/stayfit-logo.png" alt="logo">
+                    </div>
+                    <form id="dataCollectForm" class="form-horizontal">
+                        @csrf
+                        <div class="mb-3 mt-3">
+                            <label for="name" class="form-label">Enter Full Name:</label>
+                            <input type="text" class="form-control" id="name" placeholder="e.g Jhon Doe" name="name" required>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for="phone" class="form-label">Enter Mobile Number ( Please Provide WhatsApp Only ):</label>
+                            <input type="text" class="form-control" id="phone" placeholder="e.g 7896541230" name="phone" pattern="^[6789]\d{9}$" title="Phone Number Must Start With The Digit 6,7,8,9" required>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for="email" class="form-label">Enter Email:</label>
+                            <input type="email" class="form-control" id="email" placeholder="e.g jhondoe@xyz.com" name="email" required>
+                        </div>
+                        <div class="mb-3 mt-3">
+                            <label for="city" class="form-label">Enter City:</label>
+                            <input type="text" class="form-control" id="city" placeholder="e.g Guwahati" name="city" required>
+                        </div>
+                        <div class="mb-3 mt-4 text-center">
+                            <button id="continueBtn" type="submit">CONTINUE</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div> --}}
 </body>
 
-    <script>
-        $('#dataCollectForm').on('submit', function(e){
-            e.preventDefault();
+<script>
+    $('#dataCollectForm').on('submit', function(e) {
+        e.preventDefault();
 
-            $('#continueBtn').text('Please Wait....');
-            $('#continueBtn').attr('disabled', true);
+        $('#continueBtn').text('Please Wait....');
+        $('#continueBtn').attr('disabled', true);
 
-            const name = $('#name').val();
-            const phone = $('#phone').val();
-            const email = $('#email').val();
-            const city = $('#city').val();
+        const name = $('#name').val();
+        const phone = $('#phone').val();
+        const email = $('#email').val();
+        const city = $('#city').val();
 
-            sessionStorage.setItem('phone', phone);
-            sessionStorage.setItem('name', name);
-            sessionStorage.setItem('email', email);
-            sessionStorage.setItem('city', city);
+        sessionStorage.setItem('phone', phone);
+        sessionStorage.setItem('name', name);
+        sessionStorage.setItem('email', email);
+        sessionStorage.setItem('city', city);
 
 
-            $.ajax({
-                url:"{{route('send-otp')}}",
-                type:'POST',
-                data:{
-                    '_token' : "{{ csrf_token() }}",
-                    phone: phone,
-                    name : name,
-                    email : email,
-                    city:city
-                },
-                success:function(data){
-                    if(data.status === 1){
-                        Swal.fire({
-                            title: 'Great!',
-                            text: data.message,
-                            icon: 'success',
-                        }).then((result) => {
-                            window.location.replace("{{route('get-send-otp')}}");
-                        })
-                    }else{
-                        Swal.fire({
-                            title: 'Oops!',
-                            text: data.message,
-                            icon: 'error',
-                        })
+        $.ajax({
+            url: "{{ route('send-otp') }}",
+            type: 'POST',
+            data: {
+                '_token': "{{ csrf_token() }}",
+                phone: phone,
+                name: name,
+                email: email,
+                city: city
+            },
+            success: function(data) {
+                if (data.status === 1) {
+                    Swal.fire({
+                        title: 'Great!',
+                        text: data.message,
+                        icon: 'success',
+                    }).then((result) => {
+                        window.location.replace("{{ route('get-send-otp') }}");
+                    })
+                } else {
+                    Swal.fire({
+                        title: 'Oops!',
+                        text: data.message,
+                        icon: 'error',
+                    })
 
-                        $('#continueBtn').text('CONTINUE');
-                        $('#continueBtn').attr('disabled', false);
-                    }
-                },
-                error:function(xhr, error, status){
-                    console.log(error);
-
+                    $('#continueBtn').text('CONTINUE');
+                    $('#continueBtn').attr('disabled', false);
                 }
-            });
+            },
+            error: function(xhr, error, status) {
+                console.log(error);
 
-            // window.location.replace("{{route('get-send-otp')}}");
-            
+            }
         });
-    </script>
+
+        // window.location.replace("{{ route('get-send-otp') }}");
+
+    });
+</script>
+
 </html>
